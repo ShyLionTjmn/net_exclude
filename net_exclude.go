f607ec97d53d6366849bbb8ece9fcce10285267a
Sergey Klyusov <donot@email.me>
2021-09-16 14:01:29 +0500

    added support for multiple exclude networks and it may omit mask part for /32

--- net_exclude.go
+++ net_exclude.go
@@ -11,9 +11,11 @@ import (
 )
 
 var net_regex *regexp.Regexp
+var ip_or_net_regex *regexp.Regexp
 
 func init() {
   net_regex = regexp.MustCompile(`^(\d+\.\d+\.\d+\.\d+)\/(\d{1,2})$`)
+  ip_or_net_regex = regexp.MustCompile(`^(\d+\.\d+\.\d+\.\d+)(?:\/(\d{1,2}))?$`)
 }
 
 func Ip2long(ipAddr string) (uint32, error) {
@@ -87,11 +89,19 @@ func (net *Net) Split() []*Net {
   return ret
 }
 
-func (net *Net) Exclude(exclude_net *Net) {
-  if net.Contains(exclude_net) {
+func (net *Net) Exclude(exclude_nets []*Net) {
+  contains := false
+  for _, exclude_net := range exclude_nets {
+    if net.Contains(exclude_net) {
+      contains = true
+      break
+    }
+  }
+
+  if contains {
     nets := net.Split()
     for _, part := range nets {
-      part.Exclude(exclude_net)
+      part.Exclude(exclude_nets)
     }
   } else {
     fmt.Println(net)
@@ -102,8 +112,8 @@ func main() {
 
   var err error
 
-  if len(os.Args) != 3 {
-    fmt.Fprintln(os.Stderr, "USAGE: "+os.Args[0]+" n.n.n.n/n x.x.x.x/x\n\tn.n.n.n/n - network to use\n\tx.x.x.x/x - network to exclude")
+  if len(os.Args) < 3 {
+    fmt.Fprintln(os.Stderr, "USAGE: "+os.Args[0]+" n.n.n.n/n x.x.x.x/x [...]\n\tn.n.n.n/n - network to use\n\tx.x.x.x/x - networks to exclude")
     os.Exit(1)
   }
 
@@ -139,42 +149,54 @@ func main() {
     os.Exit(1)
   }
 
-  exclude_net_m := net_regex.FindStringSubmatch(os.Args[2])
-  if len(exclude_net_m) != 3 {
-    fmt.Fprintln(os.Stderr, "Bad network: "+os.Args[2])
-    os.Exit(1)
+  exclude_nets := make([]*Net, 0)
+
+  for _, arg := range os.Args[2:] {
+
+    exclude_net_m := ip_or_net_regex.FindStringSubmatch(arg)
+    if len(exclude_net_m) != 3 {
+      fmt.Fprintln(os.Stderr, "Bad network: "+arg)
+      os.Exit(1)
+    }
+
+    var exclude_addr uint32
+    exclude_addr, err = Ip2long(exclude_net_m[1])
+    if err != nil {
+      fmt.Fprintln(os.Stderr, "Bad network: "+arg)
+      os.Exit(1)
+    }
+
+    var exclude_masklen uint64
+
+    if exclude_net_m[2] != "" {
+      exclude_masklen, err = strconv.ParseUint(exclude_net_m[2], 10, 8)
+      if err != nil {
+        fmt.Fprintln(os.Stderr, "Bad network: "+arg)
+        os.Exit(1)
+      }
+    } else {
+      exclude_masklen = 32
+    }
+
+    if exclude_masklen > 32 {
+      fmt.Fprintln(os.Stderr, "Bad network: "+arg)
+      os.Exit(1)
+    }
+
+    exclude_net := &Net{exclude_addr, uint8(exclude_masklen)}
+
+    if !exclude_net.IsValid() {
+      fmt.Fprintln(os.Stderr, "Invalid network/mask: "+arg)
+      os.Exit(1)
+    }
+
+    if exclude_net.Contains(start_net) {
+      return
+    }
+
+    exclude_nets = append(exclude_nets, exclude_net)
   }
 
-  var exclude_addr uint32
-  exclude_addr, err = Ip2long(exclude_net_m[1])
-  if err != nil {
-    fmt.Fprintln(os.Stderr, "Bad network: "+os.Args[2])
-    os.Exit(1)
-  }
-
-  var exclude_masklen uint64
-  exclude_masklen, err = strconv.ParseUint(exclude_net_m[2], 10, 8)
-  if err != nil {
-    fmt.Fprintln(os.Stderr, "Bad network: "+os.Args[2])
-    os.Exit(1)
-  }
-
-  if exclude_masklen > 32 {
-    fmt.Fprintln(os.Stderr, "Bad network: "+os.Args[2])
-    os.Exit(1)
-  }
-
-  exclude_net := &Net{exclude_addr, uint8(exclude_masklen)}
-
-  if !exclude_net.IsValid() {
-    fmt.Fprintln(os.Stderr, "Invalid network/mask: "+os.Args[2])
-    os.Exit(1)
-  }
-
-  if exclude_net.Contains(start_net) {
-    return
-  }
-
-  start_net.Exclude(exclude_net)
+  start_net.Exclude(exclude_nets)
 
 }
